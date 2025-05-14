c2b223e596abfc40b644594b6d1b762314046a71
ironwaste-y9000p <ironwaste97213@outlook.com>
2025-05-14 13:06:19 +0800

    the fight ui design is complete but the panel's size is can't change with the widget

---
+++ WTF-Trans/fight-trans.py
@@ -0,0 +1,9 @@
+from PyQt5 import QtGui, QtWidgets,QtCore
+# import xlwt
+# import QCore
+import openpyxl as oxl
+import pandas as pd
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import QDate,Qt
+
+
